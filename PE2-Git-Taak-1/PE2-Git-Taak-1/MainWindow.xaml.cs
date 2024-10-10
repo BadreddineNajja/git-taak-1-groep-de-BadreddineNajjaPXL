e9569cbe74c25060dbcaa5af1416712e94fd1ab4
Badreddine Najja <Badreddine.Najja@outlook.com>
2024-10-10 02:24:30 +0200

    MOVIE-05

--- PE2-Git-Taak-1/PE2-Git-Taak-1/MainWindow.xaml.cs
+++ PE2-Git-Taak-1/PE2-Git-Taak-1/MainWindow.xaml.cs
@@ -20,5 +20,29 @@ namespace PE2_Git_Taak_1
         {
             InitializeComponent();
         }
+
+        private void Dune2_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                MovieTextBlock.Text = "Dune: Part Two\r\nPaul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family.\r\n";
+            }
+        }
+
+        private void TheSubstance_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                MovieTextBlock.Text = "The Substance\r\nA fading celebrity decides to use a black-market drug, a cell-replicating substance that temporarily creates a younger, better version of herself.\r\n";
+            }
+        }
+
+        private void Furiosa_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                MovieTextBlock.Text = "Furiosa: A Mad Max Saga\r\nThe origin story of renegade warrior Furiosa before her encounter and teamup with Mad Max.";
+            }
+        }
     }
 }
